f327475c9006e9fa19b77f08257b781da6b43e55
David Cardozo <davidcardozoemma@gmail.com>
2023-11-08 06:48:01 -0300

    Cargar estrenos de la cartelera desde la bd

---
+++ cine/cargar_cartelera_estrenos.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once "conexion.php";
+
+$sql = $conn->query("SELECT id_pelicula, nombre_imagen FROM pelicula WHERE esEstreno='si'");
+
+while ($datos = $sql->fetch()) {
+    echo "
+            <div class='col-12 col-xs-12 col-md-6 col-lg-6 col-xl-4'>
+
+                <div class='card card-individual-estreno'>
+                    <img src='" . $datos["nombre_imagen"] . "' class='card-img-top'>
+
+                    <div class='card-body'>
+                        <h5 class='card-title'>EL JUSTICIERO 3</h5>
+                        <a href='#' class='stretched-link'></a>
+                    </div>
+
+                </div>
+            </div>";
+}
+?>
