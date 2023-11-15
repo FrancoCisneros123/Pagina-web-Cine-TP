d392c5a7c7d52cd37ff70507722e707e7b19279d
David Cardozo <davidcardozoemma@gmail.com>
2023-11-15 13:10:32 -0300

    tamaño de las imagenes de las peliculas y de los estrenos separados

--- cine/cargar_cartelera_peliculas.php
+++ cine/cargar_cartelera_peliculas.php
@@ -8,7 +8,7 @@ while ($datos = $sql->fetch()) {
     echo "
         <div class='col-6 col-xs-6 col-md-6 col-lg-6 col-xl-3'>
             <div class='card card-individual'>
-                <img src='" . $datos["nombre_imagen"] . "' class='card-img-top'>
+                <img src='" . $datos["nombre_imagen"] . "' class='card-img-top imagen-peliculas'>
                 <a href='pelicula.php?id_pelicula=" . $datos["id_pelicula"] . "' class='stretched-link'></a>
             </div>
         </div>";
