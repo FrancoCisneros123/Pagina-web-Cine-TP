2aac6e9bfb371a4b2f64457a10ecf95903d20b50
Leandro-Zalazar <zalazar.leandro613@gmail.com>
2023-11-17 16:41:22 -0300

    editar gerente v1

---
+++ cine/getGerentes.php
@@ -0,0 +1,12 @@
+<?php
+require_once('conexion.php');
+
+$queryString = "SELECT * FROM usuario where id_tipo_usuario = 3";
+
+/** @var \PDO $conn */
+$sql = $conn->prepare($queryString);
+$sql->execute();
+$usuario = $sql->fetchAll(PDO::FETCH_ASSOC);
+header('Content-Type: application/json');
+echo json_encode($usuario);
+?>
