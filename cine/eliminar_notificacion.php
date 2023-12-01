9c1daeee937b65a15f369fc46b734dfe8d979d72
FrancoCisneros123 <128562613+FrancoCisneros123@users.noreply.github.com>
2023-11-30 21:00:17 -0300

    Add files via upload

---
+++ cine/eliminar_notificacion.php
@@ -0,0 +1,23 @@
+<?php 
+use controladores\NotificacionControlador;
+
+session_start();
+
+if (!isset($_SESSION["emailLogin"])) { 
+    header("location: index.php"); 
+    die(); 
+}
+
+require("MVC/controladores/permisos.php");
+$permisosObj = new Permisos();
+
+if(! $permisosObj->tienePermiso("Eliminar notificación", $_SESSION["id_usuario"])) {
+    require ("MVC/vistas/error_permiso.php");
+    die();
+}
+
+
+require ("MVC/controladores/notificacionControlador.php");
+$pelicula_controlador = new NotificacionControlador();
+$pelicula_controlador->eliminarNotificacion();
+?>
